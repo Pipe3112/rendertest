61b25ce7b40b5106d885caf9ffc96a82f7cc5967
Jonathan Castro <fc099199@gmail.com>
2025-09-30 22:39:41 -0500

    Segundo commit112

--- mycalendar/urls.py
+++ mycalendar/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path 
-from .views import * 
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import AppointmentViewSet
 
 router = DefaultRouter()
-router.register('appointments', AppointmentViewset, basename='appointments')
-urlpatterns = router.urls
+router.register('appointments', AppointmentViewSet, basename='appointments')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
